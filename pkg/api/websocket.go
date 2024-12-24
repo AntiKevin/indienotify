790e6792f6a7c18532b8b3ea83795e3fc947a9b1
antikevin <kevin_3467@yahoo.com>
2024-12-24 20:00:40 -0300

    feat: :sparkles: Adiciona módulo de websocket para a API com função de upgrade de conexão

---
+++ pkg/api/websocket.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+// modulo de websocket para a API
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func upgradeConnection(c *gin.Context) (*websocket.Conn, error) {
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return nil, err
+	}
+	return ws, nil
+}
